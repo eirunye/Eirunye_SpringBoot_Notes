a7c8587cd4318eaeb03a06e2bfda823963448450
yrung <yrungen@163.com>
2018-09-17 22:34:30 +0800

    Spring_Boot_Database_Connection

---
+++ Spring_Boot_Database_Connection/src/main/java/com/eirunye/springboot/springboot_database_connection/SpringbootDatabaseConnectionApplication.java
@@ -0,0 +1,12 @@
+package com.eirunye.springboot.springboot_database_connection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDatabaseConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDatabaseConnectionApplication.class, args);
+	}
+}
---
+++ Spring_Boot_Database_Connection/src/test/java/com/eirunye/springboot/springboot_database_connection/SpringbootDatabaseConnectionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eirunye.springboot.springboot_database_connection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootDatabaseConnectionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
