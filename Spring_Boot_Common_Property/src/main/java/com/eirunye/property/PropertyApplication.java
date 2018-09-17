8381917f679a34a39c0bc4ca2195125e41f22350
yrung <yrungen@163.com>
2018-09-17 22:47:31 +0800

    Spring_Boot_Common_Property

---
+++ Spring_Boot_Common_Property/src/main/java/com/eirunye/property/PropertyApplication.java
@@ -0,0 +1,12 @@
+package com.eirunye.property;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyApplication.class, args);
+	}
+}
---
+++ Spring_Boot_Common_Property/src/test/java/com/eirunye/property/PropertyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eirunye.property;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PropertyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
